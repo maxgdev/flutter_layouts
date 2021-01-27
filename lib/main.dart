804ce623eefb3cced5ecefa11d087b92c8403e77
maxgdev <max@maxgdev.net>
2021-01-27 20:15:14 +0000

    Final working nested rows and columns in landscape mode ONLY

--- lib/main.dart
+++ lib/main.dart
@@ -19,15 +19,23 @@ class MyApp extends StatelessWidget {
           title: Text('Flutter Layout Demo'),
         ),
         body: Center(
-          // child: Text('Hello Layout World'),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ratings
-            ],
-            
-          )
+          child: Container(
+            margin: EdgeInsets.fromLTRB(0, 40, 0, 30),
+            height: 600,
+        
+              child: Card(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Flexible(flex: 3, child: leftColumn),
+                    Flexible(flex: 4, child: mainImage),
+                  ],
+                ),
+              ),
+
           ),
+        ),
       ),
     );
   }
@@ -46,27 +54,104 @@ var stars = Row(
 );
 
 // the layout is fixed, so we can use "final"
-final ratingText =  Text(
-        '170 Reviews',
-        style: TextStyle(
-          color: Colors.black,
-          fontWeight: FontWeight.w800,
-          fontFamily: 'Roboto',
-          letterSpacing: 0.5,
-          fontSize: 20,
-        ),
-      );
+final ratingText = Text(
+  '170 Reviews',
+  style: TextStyle(
+    color: Colors.black,
+    fontWeight: FontWeight.w800,
+    fontFamily: 'Roboto',
+    letterSpacing: 0.5,
+    fontSize: 20,
+  ),
+);
 
 // the layout is fixed, so we can use "final"
 final ratings = Container(
-  padding: EdgeInsets.all(20),
+  padding: EdgeInsets.all(5),
   child: Row(
     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
     children: [
       stars,
-      ratingText, 
+      ratingText,
     ],
   ),
 );
 
 // To minimize the visual confusion that can result from heavily nested layout code, implement pieces of the UI in variables and functions.
+final descTextStyle = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w800,
+  fontFamily: 'Roboto',
+  letterSpacing: 0.5,
+  fontSize: 14,
+  height: 1.5,
+);
+
+// DefaultTextStyle.merge() allows you to create a default text
+// style that is inherited by its child and all subsequent children.
+final iconList = DefaultTextStyle.merge(
+  style: descTextStyle,
+  child: Container(
+    padding: EdgeInsets.all(10),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Column(
+          children: [
+            Icon(Icons.kitchen, color: Colors.green[500]),
+            Text('PREP:'),
+            Text('25 min'),
+          ],
+        ),
+        Column(
+          children: [
+            Icon(Icons.timer, color: Colors.green[500]),
+            Text('COOK:'),
+            Text('1 hr'),
+          ],
+        ),
+        Column(
+          children: [
+            Icon(Icons.restaurant, color: Colors.green[500]),
+            Text('FEEDS:'),
+            Text('4-6'),
+          ],
+        ),
+      ],
+    ),
+  ),
+);
+
+final descTitleStyle = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w800,
+  fontFamily: 'Roboto',
+  letterSpacing: 0.5,
+  fontSize: 16,
+  height: 2,
+);
+
+var titleText = Text("Straberry Pavlova", style: descTitleStyle,);
+var subTitle = Text(
+    "Pavlova is a merringue-based dessert named after the Russian ballerina Anua Palova.");
+
+final leftColumn = Container(
+  padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
+  child: Column(
+    children: [
+      titleText,
+      subTitle,
+      ratings,
+      iconList,
+    ],
+  ),
+);
+
+final rightColumn = Container(
+  padding: EdgeInsets.all(5.0),
+  child: Image.asset("mixed-berries.jpg"),
+);
+
+final mainImage = Container(
+  child: Image.asset('images/mixed-berries.jpg'),
+);
