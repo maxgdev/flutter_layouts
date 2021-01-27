3d5f71165a03c5ff8d824a594b0a68885f704673
maxgdev <max@maxgdev.net>
2021-01-27 16:51:23 +0000

    1st commit of flutter layout demo app

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Layout Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Layout Demo'),
+        ),
+        body: Center(child: Text('Hello Layout World'),),
+      ),
+    );
+  }
+}
+
