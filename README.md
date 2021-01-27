d604253862d5ba6449d7196ff88286c6bc76f662
maxgdev <max@maxgdev.net>
2021-01-27 17:42:13 +0000

    Added row widgets for images

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # flutter_layouts
 
-A new Flutter project.
+Flutter Layouts Demo app project.
 
 ## Getting Started
 
@@ -14,3 +14,6 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+## Credits
+- https://flutter.dev/docs/development/ui/layout
--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,17 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Flutter Layout Demo'),
         ),
-        body: Center(child: Text('Hello Layout World'),),
+        body: Center(
+          // child: Text('Hello Layout World'),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset('images/flower1.jpg'),
+              Image.asset('images/flower2.jpg'),
+              Image.asset('images/flower3.jpg'),
+            ],
+          ),
+          ),
       ),
     );
   }
