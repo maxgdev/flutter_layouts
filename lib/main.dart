51c078f8ecc740a72c87acf39d1daf6014d45490
maxgdev <max@maxgdev.net>
2021-01-27 18:42:59 +0000

    Used variables to hold layout widgets for rating widget

--- lib/main.dart
+++ lib/main.dart
@@ -23,12 +23,9 @@ class MyApp extends StatelessWidget {
           child: Row(
             mainAxisSize: MainAxisSize.min,
             children: [
-              Icon(Icons.star, color: Colors.green[500]),
-              Icon(Icons.star, color: Colors.green[500]),
-              Icon(Icons.star, color: Colors.green[500]),
-              Icon(Icons.star, color: Colors.black),
-              Icon(Icons.star, color: Colors.black),
+              ratings
             ],
+            
           )
           ),
       ),
@@ -36,3 +33,38 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// the stars colurs may change hence the use of "var"
+var stars = Row(
+  mainAxisSize: MainAxisSize.min,
+  children: [
+    Icon(Icons.star, color: Colors.green[500]),
+    Icon(Icons.star, color: Colors.green[500]),
+    Icon(Icons.star, color: Colors.green[500]),
+    Icon(Icons.star, color: Colors.black),
+    Icon(Icons.star, color: Colors.black),
+  ],
+);
+
+// the layout is fixed, so we can use "final"
+final ratingText =  Text(
+        '170 Reviews',
+        style: TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.w800,
+          fontFamily: 'Roboto',
+          letterSpacing: 0.5,
+          fontSize: 20,
+        ),
+      );
+
+// the layout is fixed, so we can use "final"
+final ratings = Container(
+  padding: EdgeInsets.all(20),
+  child: Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      stars,
+      ratingText, 
+    ],
+  ),
+);
